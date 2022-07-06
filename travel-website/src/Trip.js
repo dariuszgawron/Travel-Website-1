ab418f328d73f94032481c7bf8484cf6306d0c48
Dariusz Gawron <screbsy+g1@gmail.com>
2022-07-06 21:32:03 +0200

    Component for evaluations, code cleaning

--- travel-website/src/Trip.js
+++ travel-website/src/Trip.js
@@ -1,19 +1,5 @@
 import React from 'react';
-
-const generateStars = (maxRate,rate) => {
-    let starsRate = [];
-    for(let i=1;i<=maxRate;i++) {
-        let difference = i-rate;
-        if(difference<0.5) {
-            starsRate.push(<i className="trips-rate__icon trips-rate__icon-fill fa-solid fa-star"></i>);
-        } else if (difference>=0.5 && difference<1) {
-            starsRate.push(<i className="trips-rate__icon trips-rate__icon-fill fa-regular fa-star-half-stroke"></i>);
-        } else {
-            starsRate.push(<i className="trips-rate__icon fa-regular fa-star"></i>);
-        }
-    }
-    return starsRate;
-}
+import TripRating from './TripRating';
 
 const Trip = ({id,title,imageSrc,numberOfCountries,numberOfDays,rate,currency,minPrice,minPriceOld}) => {
     return (
@@ -28,14 +14,7 @@ const Trip = ({id,title,imageSrc,numberOfCountries,numberOfDays,rate,currency,mi
                 <h3 className='trips__title'>
                     {title}
                 </h3>
-                <div className='trips-rate'>
-                    <div className='trips-rate__box'>
-                        {generateStars(5,rate)}
-                    </div>
-                    <span className='trips-rate__average'>
-                        {rate || 0}
-                    </span>
-                </div>
+                <TripRating maxRate={5} averageRate={rate} />
                 <div className='trips__price'>
                     <span className='trips__price-base'>From {currency}{minPrice.toLocaleString('en-US')}</span>
                     <span className='trips__price-old'>{currency}{minPriceOld.toLocaleString('en-US')}</span>
--- travel-website/src/TripRating.js
+++ travel-website/src/TripRating.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-const TripRating = () => {
+const TripRating = ({maxRate=5,averageRate=3}) => {
+    let starsRate = [];
+    for(let i=1;i<=maxRate;i++) {
+        let difference = i-averageRate;
+        if(difference<0.5) {
+            starsRate.push(<i className="trips-rate__icon trips-rate__icon-fill fa-solid fa-star"></i>);
+        } else if (difference>=0.5 && difference<1) {
+            starsRate.push(<i className="trips-rate__icon trips-rate__icon-fill fa-regular fa-star-half-stroke"></i>);
+        } else {
+            starsRate.push(<i className="trips-rate__icon fa-regular fa-star"></i>);
+        }
+    }
     return (
-        ''
-    )
+        <div className='trips-rate'>
+            <div className='trips-rate__box'>
+                {starsRate}
+            </div>
+            <span className='trips-rate__average'>
+                {averageRate || 0}
+            </span>
+        </div>
+    );
 }
 
 export default TripRating;
